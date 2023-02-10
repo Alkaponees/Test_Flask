788234af7d67055a83148b1426b59674b636bf11
Alkaponees <mr.sergey1898@gmail.com>
2023-02-10 20:14:39 +0200

    Try to build Flask app with MongoDB database and add it to Docker environment

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script monitors CPU and memory usage
+while :
+do 
+  # Get the current usage of CPU and memory
+  cpuUsage=$(top -bn1 | awk '/Cpu/ { print $2}')
+  memUsage=$(free -m | awk '/Mem/{print $3}')
+  curl  -H 'Content-Type: application/json' --data '{"CPU_Usage":"${cpuUsage}","Memory_Usage":"${memUsage}"}' http://www.test.com
+  # Print the usage
+  echo "CPU Usage: $cpuUsage%"
+  echo "Memory Usage: $memUsage MB"
+ 
+  # Sleep for 1 second
+  sleep 1
+done
