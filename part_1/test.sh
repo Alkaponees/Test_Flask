bd39083f3626b6954a1c6c74286195e154062d3c
Alkaponees <mr.sergey1898@gmail.com>
2023-02-12 21:47:24 +0200

    Try to Add GET POST AND PUT

--- part_1/test.sh
+++ part_1/test.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# This script monitors CPU and memory usage
 while :
 do 
   # Get the current usage of CPU and memory
   cpuUsage=$(top -bn1 | awk '/Cpu/ { print $2}')
   memUsage=$(free -m | awk '/Mem/{print $3}')
-  curl  -H 'Content-Type: application/json' --data '{"CPU_Usage":"${cpuUsage}","Memory_Usage":"${memUsage}"}' http://www.test.com
-  # Print the usage
+  curl  -H 'Content-Type: application/json' --data '{"CPU_Usage":"${cpuUsage}","Memory_Usage":"${memUsage}"}' http://loclahost:8080/test
   echo "CPU Usage: $cpuUsage%"
   echo "Memory Usage: $memUsage MB"
  
---
+++ part_2/app.py
@@ -0,0 +1,42 @@
+import json
+from flask import Flask, request, jsonify,render_template, url_for, redirect
+from pymongo import MongoClient
+
+
+app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+db = client.flask_db
+todos = db.todos
+
+@app.route('/', methods=['GET'])
+def query_record():
+     output= []
+     for q in todos.find():
+        output.append({'CPU_Info': q.cpu_usage, 'Memory_Info':q.memory_usage})
+     #all_todos = todos.find()
+     #return render_template('index.html', todos=all_todos)
+     return jsonify({'result': output})
+     
+@app.route('/test', methods=['POST'])
+def update_record():
+    request_data = request.json
+    CPU_Usage=request_data['CPU_Usage']
+    Memory_Usage = request_data['Memory_Usage']
+    mydict={"CPU_Info":CPU_Usage, "Memory_Info": Memory_Usage}
+    print(CPU_Usage)
+    print(Memory_Usage)
+    todos.insert_one({"CPU_Info":CPU_Usage, "Memory_Info": Memory_Usage})
+    return redirect(url_for('index'))
+    #x = mydb.insert_one(mydict)
+@app.route('/update',methods=['PUT'])
+def create_record():
+    post_data=request.json
+    todos.insert_one(0, {
+        'CPU_Info' : post_data['CPU_Usage'],
+        'Memory_Info' : post_data['Memory_Usage']
+     })
+    return jsonify(post_data)
+if __name__ == "__main__":
+    app.run(Debug=True, host="0.0.0.0", port=8080) 
+    
