1adcdbf0f3da62093e1e8d7fc7ee6658ac9a2561
Alkaponees <mr.sergey1898@gmail.com>
2023-02-12 23:18:50 +0200

    Make workable our post method and test.sh files

--- part_1/test.sh
+++ part_1/test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 while :
-do 
+do
   # Get the current usage of CPU and memory
   cpuUsage=$(top -bn1 | awk '/Cpu/ { print $2}')
   memUsage=$(free -m | awk '/Mem/{print $3}')
-  curl  -H 'Content-Type: application/json' --data "{"CPU_Usage":"${cpuUsage}"),"Memory_Usage":"${memUsage}"}"  http://loclahost:8080/test
+  curl -X POST http://0.0.0.0:8080/test -H "Content-Type: application/json" --data '{"CPU_Usage":"'"$cpuUsage"'","Memory_Usage":"'"$memUsage"'"}'
   echo "CPU Usage: $cpuUsage%"
   echo "Memory Usage: $memUsage MB"
- 
+
   # Sleep for 1 second
   sleep 1
 done
--- part_2/app.py
+++ part_2/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 db = client.flask_db
 todos = db.todos
-#Workable
+
 @app.route('/', methods=['GET'])
 def query_record():
       output= []
@@ -17,26 +17,24 @@ def query_record():
      #all_todos = todos.find()
      #return render_template('index.html', todos=all_todos)
       return jsonify({'result': output})
-     
+#Workable
 @app.route('/test', methods=['POST'])
 def update_record():
-    request_data = request.json()
+    request_data = request.get_json()
     cpu_Usage=request_data['CPU_Usage']
     Memory_Usage = request_data['Memory_Usage']
     mydict={"CPU_Info":cpu_Usage, "Memory_Info": Memory_Usage}
     print(cpu_Usage)
     print(Memory_Usage)
     todos.insert_one({"CPU_Info":cpu_Usage, "Memory_Info": Memory_Usage})
-    return redirect(url_for('index'))
-    #x = mydb.insert_one(mydict)
+    return jsonify(request_data)
 @app.route('/update',methods=['PUT'])
 def create_record():
-    post_data=request.json()
+    post_data=request.json
     todos.insert_one(0, {
         'CPU_Info' : post_data['CPU_Usage'],
         'Memory_Info' : post_data['Memory_Usage']
      })
     return jsonify(post_data)
 if __name__ == "__main__":
-    app.run(Debug=True, host="0.0.0.0", port=8080) 
-    
+    app.run(Debug=True, host="0.0.0.0", port=8080)
