92830d84224cd862b6b3e46db31cadc95b1e574d
Alkaponees <mr.sergey1898@gmail.com>
2023-02-16 14:13:18 +0200

    Add file README and file app1.py for starting application localy

---
+++ README.md
@@ -0,0 +1,3 @@
+# Test_Task
+Hello everyone! It is my first try to create application that will collect system info(RAM usage, CPU usage) and save it in database(in my case - MongoDB)
+So who want to use my application, I wrote this small instruction. Let's start our journay
---
+++ part_2/app1.py
@@ -0,0 +1,46 @@
+#this file created who want to start program on local database
+import json
+from flask import Flask, request, jsonify,render_template, url_for, redirect
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+client = MongoClient('localhost', port=27017)
+db = client.flask_db
+todos = db.todos
+
+@app.route('/', methods=['GET'])
+def query_record():
+      output= []
+      for q in todos.find():
+        output.append({'_id':str(q['_id']),'CPU_Info': q['CPU_Info'], 'Memory_Info':q['Memory_Info']})
+     #all_todos = todos.find()
+     #return render_template('index.html', todos=all_todos)
+      return jsonify({'result': output})
+#Workable
+@app.route('/test', methods=['POST'])
+def create_record():
+    request_data = request.get_json()
+    cpu_Usage=request_data['CPU_Usage']
+    Memory_Usage = request_data['Memory_Usage']
+    mydict={"CPU_Info":cpu_Usage, "Memory_Info": Memory_Usage}
+    print(cpu_Usage)
+    print(Memory_Usage)
+    todos.insert_one({"CPU_Info":cpu_Usage, "Memory_Info": Memory_Usage})
+    return jsonify(request_data)
+# Workable
+@app.route('/update/<id>', methods=['PUT'])
+def update_record(id):
+    post_date=request.get_json()
+    response = todos.update_one({'_id':ObjectId(id)},{'$set':{"CPU_Info":post_date['CPU_Usage'], "Memory_Info": post_date['Memory_Usage']}})
+    if response.matched_count:
+        message = "Data updated successfully !"
+    else:
+        message = "Data not found !"
+
+    return jsonify({"result":message})
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
+#I tried to do this well
